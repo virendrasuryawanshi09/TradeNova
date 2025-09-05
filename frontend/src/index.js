2881a054ec4a218a526979310b2b1c10ae54de6b
Virendra Suryawanshi <virendrasuryawanshi805@gmail.com>
2025-09-05 07:23:18 +0530

    Creating basic files and creationg home structure

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -6,6 +6,8 @@ import './index.css';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    
+    <div>
+      
+    </div>
   </React.StrictMode>
 );
