e5ed729dc11071e198f2af71b502df0c8be952c9
Virendra Suryawanshi <virendrasuryawanshi805@gmail.com>
2025-09-05 18:37:40 +0530

    Creating frontend of award section

--- frontend/src/landing_page/home/Awards.js
+++ frontend/src/landing_page/home/Awards.js
@@ -1,7 +1,39 @@
 import React from 'react';
 function Awards() {
     return ( 
-        <h1>Awards</h1>
+        <div className='container mt-5'>
+            <div className='row'>
+                <div className='col-lg-6 p-5'>
+                    <img src='media/images/largestBroker.svg'></img>
+                </div>
+               <div className="col-6 p-5 mt-3">
+                    <h1>Largest stock broker in India.</h1>
+                    <p className='mb-5S'>
+                        2+ million Zerodha clients contribute to over 15% of all retail order
+                        volumes in India daily by trading and investing in:
+                    </p>
+
+                    <div className="row">
+                        <div className="col-6">
+                        <ul>
+                            <li><p>Futures and Options</p></li>
+                            <li><p>Commodity derivatives</p></li>
+                            <li><p>Currency derivatives</p></li>
+                        </ul>
+                        </div>
+                        <div className="col-6">
+                        <ul>
+                            <li><p>Stocks & IPOs</p></li>
+                            <li><p> Direct mutual funds</p></li>
+                            <li><p>Bonds and Govt. Securities</p></li>
+                        </ul>
+                        </div>
+                        <img src='media/images/pressLogos.png' style={{width:"9S0%"}}></img>
+                    </div>
+                </div>
+
+            </div>
+        </div>
      );
 }
 
--- frontend/src/landing_page/home/Hero.js
+++ frontend/src/landing_page/home/Hero.js
@@ -1,12 +1,12 @@
 import React from 'react'
 function Hero() {
     return ( 
-        <div className= 'container p-5'>
+        <div className= 'container p-5 mb-5'>
             <div className = 'row text-center'>
                 <img src='media/images/homeHero.png' alt='Hero_Image' className='mb-5'></img>
                 <h1 className='mt-5'>Invest in everything</h1>
                 <p></p>
-                <button className='p-2 btn btn-primary fs-5' style={{width: "17%", margin:"0 auto"}}>Signup Now</button>
+                <button className='p-2 btn btn-primary fs-5 mb-5' style={{width: "17%", margin:"0 auto"}}>Signup Now</button>
             </div>
 
         </div>
