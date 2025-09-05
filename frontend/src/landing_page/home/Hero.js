c963c5883af488e3491aa5c0c2c2098950606b0e
Virendra Suryawanshi <virendrasuryawanshi805@gmail.com>
2025-09-05 17:54:59 +0530

    Created frontend of home page

--- frontend/src/landing_page/home/Hero.js
+++ frontend/src/landing_page/home/Hero.js
@@ -1,7 +1,15 @@
 import React from 'react'
 function Hero() {
     return ( 
-        <h1>Hero</h1>
+        <div className= 'container p-5'>
+            <div className = 'row text-center'>
+                <img src='media/images/homeHero.png' alt='Hero_Image' className='mb-5'></img>
+                <h1 className='mt-5'>Invest in everything</h1>
+                <p></p>
+                <button className='p-2 btn btn-primary fs-5' style={{width: "17%", margin:"0 auto"}}>Signup Now</button>
+            </div>
+
+        </div>
     );
 }
 
