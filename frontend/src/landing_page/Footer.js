3d5514844f047639a2317e5d07232368c1e7e851
Virendra Suryawanshi <virendrasuryawanshi805@gmail.com>
2025-09-06 15:51:45 +0530

    Completing Footer

--- frontend/src/landing_page/Footer.js
+++ frontend/src/landing_page/Footer.js
@@ -1,8 +1,90 @@
-import React from 'react'
+import React from 'react';
 
 function Footer() {
-    return ( 
-        <h1>Footer</h1>
+    return (
+        <footer style={{ backgroundColor: "#FBFBFB" }}>
+            <div className="container border-top mt-5">
+                <div className="row mt-5">
+                    <div className="col">
+                        <img src="media/images/logo.svg" style={{ width: "50%" }} alt="TradeNova Logo" />
+                        <p>&copy;2025, TradeNova Broking Ltd. <br />All rights reserved.</p>
+                    </div>
+                    <div className="col">
+                        <p><strong>Account</strong></p>
+                        <p><a href="">Open demat account</a></p>
+                        <p><a href="">Minor demat account</a></p>
+                        <p><a href="">NRI demat account</a></p>
+                        <p><a href="">Commodity</a></p>
+                        <p><a href="">Dematerialisation</a></p>
+                        <p><a href="">Fund transfer</a></p>
+                        <p><a href="">MTF</a></p>
+                        <p><a href="">Referral program</a></p>
+                    </div>
+                    <div className="col">
+                        <p><strong>Support</strong></p>
+                        <p><a href="">Support portal</a></p>
+                        <p><a href="">Contact us</a></p>
+                        <p><a href="">How to file a complaint?</a></p>
+                        <p><a href="">Status of your complaints</a></p>
+                        <p><a href="">Bulletin</a></p>
+                        <p><a href="">Circular</a></p>
+                        <p><a href="">Blog</a></p>
+                        <p><a href="">Downloads</a></p>
+                    </div>
+                    <div className="col">
+                        <p><strong>Company</strong></p>
+                        <p><a href="">About</a></p>
+                        <p><a href="">Philosophy</a></p>
+                        <p><a href="">Press & media</a></p>
+                        <p><a href="">Careers</a></p>
+                        <p><a href="">TradeNova Cares (CSR)</a></p>
+                        <p><a href="">TradeNova.tech</a></p>
+                        <p><a href="">Open source</a></p>
+                    </div>
+                    <div className="col">
+                        <p><strong>Quick Links</strong></p>
+                        <p><a href="">Upcoming IPOs</a></p>
+                        <p><a href="">Brokerage charges</a></p>
+                        <p><a href="">Market holidays</a></p>
+                        <p><a href="">Economic calendar</a></p>
+                        <p><a href="">Calculators</a></p>
+                        <p><a href="">Markets</a></p>
+                        <p><a href="">Sectors</a></p>
+                    </div>
+                </div>
+
+                <div className="mt-5 text-small text-muted" style={{ fontSize: "12px" }}>
+                    <p>
+                        TradeNova Broking Ltd.: Member of NSE, BSE​ &​ MCX – SEBI Registration no.: INZ000031633 
+                        CDSL/NSDL: Depository services through TradeNova Broking Ltd. – SEBI Registration no.: IN-DP-431-2019 
+                        Commodity Trading through TradeNova Commodities Pvt. Ltd. MCX: 46025; SEBI Registration no.: INZ000038238 
+                        Registered Address: TradeNova Broking Ltd., #153/154, 4th Cross, Dollars Colony, Opp. Clarence Public School, 
+                        J.P Nagar 4th Phase, Bengaluru - 560078, Karnataka, India. 
+                        For any complaints pertaining to securities broking please write to 
+                        <a href="mailto:complaints@tradenova.com"> complaints@tradenova.com</a>, 
+                        for DP related to <a href="mailto:dp@tradenova.com">dp@tradenova.com</a>. 
+                        Please ensure you carefully read the Risk Disclosure Document as prescribed by SEBI | ICF
+                    </p>
+                    <p>Procedure to file a complaint on SEBI SCORES: Register on SCORES portal. Mandatory details: Name, PAN, Address, Mobile Number, E-mail ID. Benefits: Effective Communication, Speedy redressal of grievances</p>
+                    <p>Smart Online Dispute Resolution | Grievances Redressal Mechanism</p>
+                    <p>Investments in securities market are subject to market risks; read all related documents carefully before investing.</p>
+                    <p>Attention investors: 1) Stock brokers can accept securities as margins from clients only by way of pledge in the depository system w.e.f September 01, 2020. 2) Update your e-mail and phone number with your stock broker / depository participant and receive OTP directly from depository on your e-mail and/or mobile number to create pledge. 3) Check your securities / MF / bonds in the consolidated account statement issued by NSDL/CDSL every month.</p>
+                    <p>India's largest broker based on networth as per NSE. NSE broker factsheet</p>
+                    <p>
+                        "Prevent unauthorised transactions in your account. Update your mobile numbers/email IDs with your stock brokers. 
+                        Receive information of your transactions directly from Exchange on your mobile/email at the end of the day. 
+                        Issued in the interest of investors. KYC is one time exercise while dealing in securities markets - once KYC is done 
+                        through a SEBI registered intermediary (broker, DP, Mutual Fund etc.), you need not undergo the same process again 
+                        when you approach another intermediary."  
+                        Dear Investor, if you are subscribing to an IPO, there is no need to issue a cheque. Please write the Bank account number 
+                        and sign the IPO application form to authorize your bank to make payment in case of allotment. In case of non-allotment 
+                        the funds will remain in your bank account.  
+                        As a business we don't give stock tips, and have not authorized anyone to trade on behalf of others. 
+                        If you find anyone claiming to be part of TradeNova and offering such services, please create a ticket here.
+                    </p>
+                </div>
+            </div>
+        </footer>
     );
 }
 
--- frontend/src/landing_page/OpenAccount.js
+++ frontend/src/landing_page/OpenAccount.js
@@ -4,7 +4,7 @@ function OpenAccount() {
    return ( 
         <div className= 'container p-5 mb-5'>
             <div className = 'row text-center'>
-                <h2 className='mt-6'>Open a Zerodha account</h2>
+                <h2 className='mt-6'>Open a TradeNova account</h2>
                 <p className='mt-4'>Modern platform and apps, ₹ 0 investments, and flat ₹ 20 intraday and F&O trades.</p>
                 <button className='p-2 btn btn-primary mt-4 fs-5 mb-5' style={{width: "17%", margin:"0 auto"}}>Sign up Now</button>
             </div>
