9cfa312ad2d0580b7efd8c4c7ead679c98ba19d2
Virendra Suryawanshi <virendrasuryawanshi805@gmail.com>
2025-09-06 14:27:31 +0530

    Settingup Navbar

--- frontend/src/landing_page/Navbar.js
+++ frontend/src/landing_page/Navbar.js
@@ -1,9 +1,52 @@
-import React from 'react'
+import React from "react";
 
 function Navbar() {
-    return ( 
-        <h1>Navbar</h1>
-    );
+  return (
+    <nav className="navbar navbar-expand-lg navbar-light bg-white shadow-sm">
+      {/* 🔹 Logo */}
+      <a className="navbar-brand" href="#">
+        <img
+          src="media/images/logo.svg"
+          alt="Zerodha Logo"
+          style={{ width: "130px" }}
+        />
+      </a>
+
+      {/* 🔹 Toggler (for mobile) */}
+      <button
+        className="navbar-toggler"
+        type="button"
+        data-bs-toggle="collapse"
+        data-bs-target="#navbarNav"
+        aria-controls="navbarNav"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span className="navbar-toggler-icon"></span>
+      </button>
+
+      {/* 🔹 Nav links aligned to right */}
+      <div className="collapse navbar-collapse justify-content-end" id="navbarNav">
+        <ul className="navbar-nav">
+          <li className="nav-item">
+            <a className="nav-link" href="#">Signup</a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="#">About</a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="#">Products</a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="#">Pricing</a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="#">Support</a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
 }
 
 export default Navbar;
