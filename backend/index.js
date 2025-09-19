fd91d3eeb362d4f35ab0dbb99b2e4ff66e93316d
Virendra Suryawanshi <virendrasuryawanshi805@gmail.com>
2025-09-19 15:06:55 +0530

    Creating basic schemas and models

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,12 @@
+require("dotenv").config();
 const express = require("express");
+const mongoose = require("mongoose");
+
+const PORT = process.env.PORT || 3002;
+const MONGO_URL = process.env.MONGO_URL;
 
 const app = express();
+mongoose.connect(MONGO_URL);
 
 app.listen(3002, ()=> {
     console.log("Server is running on port 3002");
